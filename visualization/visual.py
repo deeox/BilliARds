14b3b602b2fb904e5446a52013e943f2045522e0
DipakDA <dipak19da@gmail.com>
2019-01-02 11:01:30 +0530

    Added the visual.py file

---
+++ visualization/visual.py
@@ -0,0 +1,28 @@
+import Tkinter
+
+V1x, V1y = 10, 10
+V4x, V4y = 310, 610
+
+canvas_width = 1000
+canvas_height = 1000
+
+top = Tkinter.Tk()
+canvas = Tkinter.Canvas(top, width = canvas_width, height = canvas_height)
+main_table = canvas.create_rectangle(V1x, V1y, V4x, V4y, width=3)
+
+#bottom-left-hole
+canvas.create_arc(-10, 590, 30, 630, width=3)
+#bottom-right-hole
+canvas.create_arc(330, 590, 290, 630,width=3, start=90)
+#top-left-hole
+canvas.create_arc(-10, 30, 30, -10, width=3, start=270)
+#top-right-hole
+canvas.create_arc(290, -10, 330, 30, width=3, start=180)
+#mid-left-hole
+canvas.create_arc(10, 310, 10, 350, width=3, start = 270)
+
+#canvas.create_oval(310, 590, 310, 590, width = 10, fill = 'red')
+
+canvas.pack()
+top.title("BilliARds")
+top.mainloop()
